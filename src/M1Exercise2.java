368c9b1e5ebaba3355deceff6fc47219a5a0e6cc
hamza <hyousaf@ivytech.edu>
2019-09-06 10:28:20 -0400

    First program M01 EX2 2.3

--- src/M1Exercise2.java
+++ src/M1Exercise2.java
@@ -1,6 +1,24 @@
+// Hamza Yousaf
+// SDEV JAVA
+// Program demands input from user of a number it will convert and display in meters
+// last modified: 9/6/2019
+
 import java.util.Scanner;
 public class M1Exercise2 {
     public static void main(String[] args) {
+        System.out.println("This program will convert you value(ft) in meters(m)");
+        System.out.println("Enter a number that you want to convert = ");
+
+double num;
+double meters;
+
+Scanner input = new Scanner(System.in);
+num = input.nextDouble();
+
+meters = num * 0.305;
+
+        System.out.println("The number you entered is = " + num +"ft");
+        System.out.println("Your number in meters is = " + meters +"m");
 
     }
 }
